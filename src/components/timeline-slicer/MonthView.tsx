fcb735cf5fb65ecf22c44e4643de0f33f8a05929
Shyam <shyam.panchal@truboardpartners.com>
2022-11-15 16:50:14 +0530

    Month Wise Slicer 90% working

--- src/components/timeline-slicer/MonthView.tsx
+++ src/components/timeline-slicer/MonthView.tsx
@@ -3,7 +3,12 @@ import { Panel } from "rsuite";
 import { InputUI } from "./InputUI";
 import { Calender, monthNames } from "./utils";
 
-export function MonthView(props: { calender: Calender; sliderMax: number }) {
+export function MonthView(props: {
+  calender: Calender;
+  sliderMax: number;
+  startDate?: Date;
+  endDate?: Date;
+}) {
   const ref: any = React.useRef(null);
   const [rangeWidth, setRangeWidth] = React.useState<number>(0);
   React.useEffect(() => {
@@ -12,10 +17,58 @@ export function MonthView(props: { calender: Calender; sliderMax: number }) {
       setRangeWidth(ref.current.offsetWidth);
     }
   }, [ref.current]);
+
+  let months = Object.values(props.calender.years)
+    .map((year) =>
+      Object.values(year.quaters).map((quater) =>
+        Object.entries(quater.months).map(([month, monthData]) => ({
+          month,
+          ...monthData,
+        }))
+      )
+    )
+    .flat()
+    .flat();
+
+  console.log("Months", months);
+
+  const [startDate, setStartDate] = React.useState<Date>(
+    props.startDate ? props.startDate : props.calender.startDate
+  );
+  const [endDate, setEndDate] = React.useState<Date>(
+    props.endDate ? props.endDate : props.calender.endDate
+  );
+
+  const updateStartIndex = (index: number) => {
+    const month = months[index - 1];
+    setStartDate(month.startDate);
+  };
+
+  const updateEndIndex = (index: number) => {
+    const month = months[index - 2];
+    setEndDate(month.endDate);
+  };
+
+  const bottomRef: any = React.useRef(null);
+
+  React.useEffect(() => {
+    // 👇️ scroll to bottom every time messages change
+    bottomRef.current?.scrollIntoView({
+      behavior: "auto",
+      // behavior: "smooth"
+    });
+  }, [bottomRef.current]);
+
   return (
     <div style={{ position: "relative", overflowY: "hidden" }}>
-      <InputUI sliderMin={1} sliderMax={props.sliderMax} width={rangeWidth} />
-      <div className="ui-container">
+      <InputUI
+        sliderMin={1}
+        sliderMax={props.sliderMax}
+        width={rangeWidth}
+        setStartIndex={updateStartIndex}
+        setEndIndex={updateEndIndex}
+      />
+      <div className="ui-container" ref={ref}>
         {Object.entries(props.calender.years).map(([year, quaters]) =>
           Object.entries(quaters.quaters).map(([quater, months]) => (
             <Panel
@@ -31,12 +84,18 @@ export function MonthView(props: { calender: Calender; sliderMax: number }) {
             >
               {year} - Q{quater}
               <div className="sub-container">
-                {Object.keys(months.months).map((month) => {
+                {Object.entries(months.months).map(([month, _monthData]) => {
+                  const selected =
+                    startDate <= _monthData.endDate &&
+                    endDate >= _monthData.startDate;
                   return (
                     <Panel
                       key={month}
                       className="year-box flex-box"
-                      style={{ minWidth: "50px" }}
+                      style={{
+                        minWidth: "50px",
+                        backgroundColor: selected ? "#c8c8c8" : "transparent",
+                      }}
                       shaded
                       bordered
                       bodyFill
@@ -49,6 +108,7 @@ export function MonthView(props: { calender: Calender; sliderMax: number }) {
             </Panel>
           ))
         )}
+        <div ref={bottomRef} />
       </div>
     </div>
   );
